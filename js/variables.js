326beef80c3635a0b5711809d3f9f721cb6cd062
Gursewak singh <98080065+Singh1356@users.noreply.github.com>
2023-11-04 14:54:51 +0530

    Create variables.js

---
+++ js/variables.js
@@ -0,0 +1,12 @@
+const accountId = 135600;
+let accountPassword = 400564;
+var accountEmail = "gsj620@hotmail.com";
+accountCity = "hanumnagrah";
+accountState;
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
